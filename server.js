fc344ffd1df193957c6457e38a37012465f52f9d
Trevor Pilgrim <pilg5971@stthomas.edu>
2021-02-15 23:29:59 -0600

    Updated Server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || "8000";
+const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello World!");
